66b79470f2159e5a1df8afef90d3b8ab925d0aee
Rohan S Kamath <123623049+rohanskamath@users.noreply.github.com>
2025-02-24 15:38:02 +0530

    Delete nodebackend directory

--- nodebackend/routes/PinRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const pinController = require("../controllers/pinController");
-
-/* Public Routes */
-router.post("/", pinController.createPin);
-router.get("/", pinController.getAllPins);
-
-module.exports = router;
